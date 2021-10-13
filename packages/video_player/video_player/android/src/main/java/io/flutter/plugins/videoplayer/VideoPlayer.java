2d7942527b83a55b579410ed43f29f935a3725b6
Israel Madueme <purelogiq@gmail.com>
2021-10-12 22:41:20 -0400

    fix types

--- packages/video_player/video_player/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
+++ packages/video_player/video_player/android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayer.java
@@ -61,10 +61,10 @@ final class VideoPlayer {
   private final VideoPlayerOptions options;
 
   // ReactFans Patch - reduce initial buffer for playback time
-  static const RE_DEFAULT_MIN_BUFFER_MS = 5000; // 5 sec.
-  static const RE_DEFAULT_MAX_BUFFER_MS = 20000; // 20 sec. No need to download full video if user is not going to watch it all.
-  static const RE_DEFAULT_BUFFER_FOR_PLAYBACK_MS = 1000; // 1 sec.
-  static const RE_DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS = 2000; // 2 sec.
+  static const int RE_DEFAULT_MIN_BUFFER_MS = 5000; // 5 sec.
+  static const int RE_DEFAULT_MAX_BUFFER_MS = 20000; // 20 sec. No need to download full video if user is not going to watch it all.
+  static const int RE_DEFAULT_BUFFER_FOR_PLAYBACK_MS = 1000; // 1 sec.
+  static const int RE_DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS = 2000; // 2 sec.
   // End Patch
 
   VideoPlayer(
